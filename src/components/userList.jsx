50da66c58a31a0ee0727b2b8d5fb7cf26383f0c7
Beto Cortés <roberto.cortes13233@gmail.com>
2021-04-12 15:05:17 -0500

    filter prototype 2, this could work

--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { GlobalContext } from '../Context'
 import '../styles/userList.css'
 import BoxFilter from './BoxFilter'
@@ -8,15 +8,25 @@ export default function UserList() {
 
   const [store] = useContext(GlobalContext)
   const { users, characteristics } = store
+  const [usersSelected, setUsersSelected] = useState([])
 
-  const usersFiltered = users.filter((user) => {
-    return user.role === 'Frontend'
-  })
-    .map((user) => {
-      return user
+  useEffect(() => {
+    characteristics.map((item) => {
+      users.filter((user) => {
+        if (user.role === item) {
+          console.log(user.role === item, 'los roles coinciden')
+          setUsersSelected([user])
+          console.log(usersSelected)
+          return user.role === item
+        } if (user.level === item) {
+          return user.level === item
+        }
+        return user
+      })
     })
+  }, [characteristics])
 
-  console.log(usersFiltered)
+  console.log('Usuarios en estado:  ', usersSelected)
 
   return (
     <section className="user__list-wrapper">
